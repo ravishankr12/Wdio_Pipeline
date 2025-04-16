22c4d3f6b5ae925c0247e5894ee0c79a9ca01d27
ravitest184 <203852688+ravishankr12@users.noreply.github.com>
2025-04-16 14:36:05 +0530

    Allure reporting

--- wdio.conf.js
+++ wdio.conf.js
@@ -48,7 +48,13 @@ export const config = {
   connectionRetryTimeout: 120000,
   connectionRetryCount: 3,
   framework: "mocha",
-  reporters: ["spec"],
+  
+  reporters: ["spec", ['allure', {
+    outputDir: 'allure-results',
+    disableWebdriverStepsReporting: true,
+    disableWebdriverScreenshotsReporting: true,
+}]],
+
   mochaOpts: {
     ui: "bdd",
     timeout: 60000,
