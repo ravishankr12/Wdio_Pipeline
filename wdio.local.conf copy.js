df3729992bd9ddf99d17230e97c7b23da42f1aac
ravitest184 <203852688+ravishankr12@users.noreply.github.com>
2025-04-23 11:32:31 +0530

    Parallel execution in the local env.

---
+++ wdio.local.conf copy.js
@@ -0,0 +1,62 @@
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const config = {
+   runner: "local",
+
+  //Browser_Stack credentials 
+  //WHEN WE PASS THE VALUE FROM THE CONFIG FILE TO BROWSERSTACK
+  // user: "ravishanker_jvX2ZU",
+  // key: "kqRrbsxx2WNJzhtyVget",
+
+  // user: process.env.BROWSERSTACK_USER,
+  // key: process.env.BROWSERSTACK_KEY,
+  // user: process.env.BS_USER,
+  // key: process.env.BS_KEY,
+
+
+
+  specs: ["./test/specs/android/UI_E2E.spec.js"],
+  exclude: [],
+  maxInstances: 10,
+  services: ["appium"],
+  hostname: "localhost",
+  port: 4723,
+  path: "/wd/hub",
+  capabilities: [
+    {
+      "appium:platformName": "Android",
+      //"appium:deviceName": "Google Pixel 9",
+      "appium:deviceName": "emulator-5554",
+      "appium:platformVersion": "11.0",
+      "appium:automationName": "UiAutomator2",
+      "appium:app": path.join(process.cwd(), "app/android/SauceLabs.apk"),
+      "appium:autoGrantPermissions": true,
+      "appium:appWaitActivity": "*",
+      // "bstack:options": {
+      //   "projectName": "My Android E2E Tests",
+      //   "buildName": "Build 1.0",
+      //   "sessionName": "UI E2E Flow",
+      //   "idleTimeout": 300
+      // }
+    }
+  ],
+
+  logLevel: "info",
+  bail: 0,
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  framework: "mocha",
+  reporters: ["spec", ['allure', {
+    outputDir: 'allure-results',
+    disableWebdriverStepsReporting: true,
+    disableWebdriverScreenshotsReporting: true,
+}]],
+
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 60000,
+  },
+};
--- wdio.local.conf.js
+++ wdio.local.conf.js
@@ -3,57 +3,58 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-   runner: "local",
+  runner: "local",
 
-  //Browser_Stack credentials 
-  //WHEN WE PASS THE VALUE FROM THE CONFIG FILE TO BROWSERSTACK
-  // user: "ravishanker_jvX2ZU",
-  // key: "kqRrbsxx2WNJzhtyVget",
+  specs: ["./test/specs/android/*.spec.js"],
 
-  // user: process.env.BROWSERSTACK_USER,
-  // key: process.env.BROWSERSTACK_KEY,
-  // user: process.env.BS_USER,
-  // key: process.env.BS_KEY,
-
-
-
-  specs: ["./test/specs/android/UI_E2E.spec.js"],
   exclude: [],
-  maxInstances: 10,
-  services: ["appium"],
-  hostname: "localhost",
-  port: 4723,
-  path: "/wd/hub",
+
+  maxInstances: 2, // Run in parallel on two emulators
+
   capabilities: [
     {
       "appium:platformName": "Android",
-      //"appium:deviceName": "Google Pixel 9",
-      "appium:deviceName": "emulator-5554",
+      "appium:deviceName": "emulator-5554", // First emulator
       "appium:platformVersion": "11.0",
       "appium:automationName": "UiAutomator2",
       "appium:app": path.join(process.cwd(), "app/android/SauceLabs.apk"),
       "appium:autoGrantPermissions": true,
       "appium:appWaitActivity": "*",
-      // "bstack:options": {
-      //   "projectName": "My Android E2E Tests",
-      //   "buildName": "Build 1.0",
-      //   "sessionName": "UI E2E Flow",
-      //   "idleTimeout": 300
-      // }
+    }
+    {
+      "appium:platformName": "Android",
+      "appium:deviceName": "emulator-5556", // Second emulator
+      "appium:platformVersion": "16.0", // Adjust to match your second emulator
+      "appium:automationName": "UiAutomator2",
+      "appium:app": path.join(process.cwd(), "app/android/SauceLabs.apk"),
+      "appium:autoGrantPermissions": true,
+      "appium:appWaitActivity": "*",
     }
   ],
 
+  services: ["appium"],
+
+  hostname: "localhost",
+  port: 4723,
+  path: "/wd/hub",
+
   logLevel: "info",
+
   bail: 0,
   waitforTimeout: 10000,
   connectionRetryTimeout: 120000,
   connectionRetryCount: 3,
+
   framework: "mocha",
-  reporters: ["spec", ['allure', {
-    outputDir: 'allure-results',
-    disableWebdriverStepsReporting: true,
-    disableWebdriverScreenshotsReporting: true,
-}]],
+
+  reporters: [
+    "spec",
+    ["allure", {
+      outputDir: "allure-results",
+      disableWebdriverStepsReporting: true,
+      disableWebdriverScreenshotsReporting: false,
+    }]
+  ],
 
   mochaOpts: {
     ui: "bdd",
